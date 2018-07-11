7c0615a114250494019d4d4e518aaca1d7c7e7b1
Egill Sig <egillsig@Workaholic.techsupport.is>
2018-07-11 14:35:30 +0000

    fir

---
+++ woocommerce-easy-sale-date-price.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Woocommerce Easy Sale Date Price
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  This plugin 
+Version:      1.0.0
+Author:       Egill Sigurðarson
+Author URI:   https://egillspegill.is/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  Egill
+Domain Path:  /languages
+*/
