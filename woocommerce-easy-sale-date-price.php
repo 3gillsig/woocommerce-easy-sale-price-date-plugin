9bd8d8084abcd8932fc5f8795d9bbb1f605ff8f4
Egill Sig <egillsig@Workaholic.techsupport.is>
2018-07-12 10:15:04 +0000

    basic

--- woocommerce-easy-sale-date-price.php
+++ woocommerce-easy-sale-date-price.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name:  Woocommerce Easy Sale Date Price
-Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Plugin URI:   https://github.com/3gillsig/woocommerce-easy-sale-price-date-plugin.git
 Description:  This plugin 
 Version:      1.0.0
 Author:       Egill Sigurðarson
@@ -11,3 +11,47 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  Egill
 Domain Path:  /languages
 */
+
+if (! defined('ABSPATH')){
+    die;
+    exit;
+}
+
+defined( 'ABSPATH') or die('Hey, can´t access this file, silly human!');
+ if(file_exists(dirname(__FILE__) . '/vendor/autoload.php')){
+     require_once dirname(__FILE__) . '/vendor/autoload.php';
+ }
+
+ if(! class_exists('woocommerce_easy_sale_date_price')) {
+
+ }
+
+class woocommerce_easy_sale_date_price {
+    
+    function change_date($array){
+        if($array) {
+            $prods = wc_get_products( );
+          
+            foreach($prods as $prod){
+                $arg= $prod['price'];
+                $arg=$arg*(1+($prosentage/100));
+                $prod['sale_price']= $arg;
+            }
+            return $prods;
+        }
+    }
+    function change_price($prosentage){
+        if(100 > $prosentage && $prosentage > 0) {
+          $prods = wc_get_products( );
+          
+            foreach($prods as $prod){
+                $arg= $prod['price'];
+                $arg=$arg*(1+($prosentage/100));
+                $prod['sale_price']= $arg;
+            }
+            return $prods;
+        }
+    }
+    
+
+}
