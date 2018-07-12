c21617dd1b3730b17390fa77b8a106d7a12f0f3f
Egill Sig <egillsig@Workaholic.techsupport.is>
2018-07-12 17:01:54 +0000

    admin pages

--- woocommerce-easy-sale-date-price.php
+++ woocommerce-easy-sale-date-price.php
@@ -58,5 +58,22 @@ class woocommerce_easy_sale_date_price {
             change_date($openingDate, $closingDate);
         }
     }
-
+    function register(){
+        add_action( 'admin_enqueue_scripts', array($this, 'enqueue'));
+        add_action( 'wp_enqueue_scripts', array($this, 'enqueue'));
+        add_action( 'admin_menu', array($this, 'add_admin_pages') );
+        add_filter( "plugin_action_links_$this->plugin", array($this, 'settings_link' ));
+    }
+    public function settings_link($links){
+        $setting_link = '<a href="admin.php?page=EasySaleDatePrice">Settings</a>';
+        array_push($links, $setting_link);
+        return $links;
+    }
+    function add_admin_pages(){
+        add_menu_page( 'Woocommerce Easy Sale Date Price', 'Easy Sale Date Price', 'manage_options', 'EasySaleDatePrice', array($this, 'admin_index'), 'dashicons-store', 110 );
+    }
+    function enqueue(){
+        wp_enqueue_style( 'mypluginstyle', plugins_url( ('/assets/style.css'), __FILE__ ) );
+        wp_enqueue_script( 'mypluginstyle', plugins_url( ('/assets/script.js'), __FILE__ ) );
+    }
 }
